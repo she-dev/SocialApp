4e7ccade22abdafbf508aacea2e8001cd1b89669
Heloá Meneses <heloa.meneses@gmail.com>
2017-01-08 20:39:33 -0200

    Adding application root

--- index.ios.js
+++ index.ios.js
@@ -1,53 +1,28 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
-  Text,
   View
 } from 'react-native';
+import RootNavigator from './src/views/RootNavigator';
+
+const styles = {
+  containerStyle: {
+    flex: 1,
+    // justifyContent: 'center',
+    // alignItems: 'center',
+  }
+};
 
 export default class SocialApp extends Component {
   render() {
+    const { containerStyle } = styles;
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+      <View style={containerStyle}>
+        <RootNavigator ref="rootNavigator" />
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('SocialApp', () => SocialApp);
